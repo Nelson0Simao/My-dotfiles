91db043ba838e65ee05b9ded165062903611f0ae
Nelson Simão <nelsonsimao200@gmail.com>
2025-02-01 11:09:50 +0100

    alias git

---
+++ git-alias.zsh
@@ -0,0 +1,17 @@
+# talhos para Git
+alias gs='git status'          # Ver status do repositório
+alias ga='git add'             # Adicionar arquivos ao staging
+alias gc='git commit -m'       # Fazer commit com mensagem
+alias gca='git commit -am'     # Adicionar todos os arquivos e fazer commit com mensagem
+alias gpush='git push'         # Enviar commits para o repositório remoto
+alias gpull='git pull'         # Atualizar repositório local com alterações remotas
+alias gco='git checkout'       # Mudar de branch
+alias gb='git branch'          # Listar branches
+alias gd='git diff'            # Ver diferenças entre arquivos
+alias gl='git log --oneline'   # Ver histórico de commits (resumido)
+alias gcl='git clone'          # Clonar um repositório
+alias gst='git stash'          # Guardar alterações temporariamente
+alias gsp='git stash pop'      # Aplicar alterações guardadas
+alias gr='git restore'         # Restaurar arquivos (desfazer alterações)
+alias gf='git fetch'           # Buscar alterações do repositório remoto
+alias greset='git reset --hard HEAD'  # Resetar para o último commit (cuidado!)
